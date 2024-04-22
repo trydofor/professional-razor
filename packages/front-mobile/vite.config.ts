d5a95d2310f16822b792bfb9c2301f153acebdf4
trydofor <trydofor@gmail.com>
2024-04-22 09:02:12 +0800

    🎨 share favicon.png #18

--- packages/front-mobile/vite.config.ts
+++ packages/front-mobile/vite.config.ts
@@ -1,21 +1,14 @@
-import legacy from '@vitejs/plugin-legacy'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
-import { defineConfig } from 'vite'
+import legacy from '@vitejs/plugin-legacy';
+import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import DynamicPublicDirectory from 'vite-multiple-assets';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    legacy()
-  ],
+  plugins: [vue(), legacy(), DynamicPublicDirectory(['../shared/public'])],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': resolve(__dirname, 'src'),
     },
   },
-  test: {
-    globals: true,
-    environment: 'jsdom'
-  }
-})
+});

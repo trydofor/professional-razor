c991d7c46b4fdcbdf7dae118e236d141dba1b612
trydofor <trydofor@gmail.com>
2024-09-15 20:39:27 +0800

    ✨ DpiImg and lint #4

---
+++ .changeset/twenty-clocks-deny.md
@@ -0,0 +1,6 @@
+---
+"@fessional/razor-common": patch
+"@fessional/razor-mobile": patch
+---
+
+add DpiImg, lint at top
---
+++ layers/common/components/DpiImg.vue
@@ -0,0 +1,17 @@
+<template>
+  <img
+    :srcset="srcset"
+    :src="src"
+    :alt="props.alt"
+  >
+</template>
+
+<script lang="ts" setup>
+const props = defineProps<{
+  alt?: string;
+  srcset: string[];
+}>();
+
+const src = props.srcset[0];
+const srcset = props.srcset.map((img, idx) => `${img} ${idx + 1}x`).join(', ');
+</script>

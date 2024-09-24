c6b9839f425297ef0538cfcc003cf89d0ac2ad7e
trydofor <trydofor@gmail.com>
2024-09-24 08:45:43 +0800

    ✨ typed storage as vueuse core #6

---
+++ layers/mobile/test/typed-storage.test.ts
@@ -0,0 +1,56 @@
+﻿import { flushPromises } from '@vue/test-utils';
+import { expect, test } from 'vitest';
+import { defStorage, useStorage } from '../utils/typed-storage';
+
+test('defineStorage setter', () => {
+  const storage = defStorage<string>({ key: 'test1' });
+  expect(storage.value).toEqual(Promise.resolve(null));
+
+  storage.value = '1';
+  expect(storage.value).toEqual(Promise.resolve('1'));
+
+  storage.value = null;
+  expect(storage.value).toEqual(Promise.resolve(null));
+
+  storage.value = Promise.resolve('2');
+  expect(storage.value).toEqual(Promise.resolve('2'));
+
+  storage.value = Promise.resolve(null);
+  expect(storage.value).toEqual(Promise.resolve(null));
+});
+
+test('defineStorage callback', async () => {
+  const ref = { value: '0' };
+  const storage = defStorage<string>({ key: 'test1', callback: v => (ref.value = v ?? '') });
+  expect(storage.value).toEqual(Promise.resolve(null));
+
+  await flushPromises();
+  expect(ref.value).toEqual('');
+
+  storage.value = '1';
+  await flushPromises();
+  expect(ref.value).toEqual('1');
+
+  storage.value = null;
+  await flushPromises();
+  expect(ref.value).toEqual('');
+
+  storage.value = Promise.resolve('2');
+  await flushPromises();
+  expect(ref.value).toEqual('2');
+
+  storage.value = Promise.resolve(null);
+  await flushPromises();
+  expect(ref.value).toEqual('');
+});
+
+test('useStorage', () => {
+  const storage = useStorage<string>({ key: 'test2', init: '0' });
+  expect(storage.value).toEqual('0');
+
+  storage.value = '1';
+  expect(storage.value).toEqual('1');
+
+  storage.value = null;
+  expect(storage.value).toEqual(null);
+});

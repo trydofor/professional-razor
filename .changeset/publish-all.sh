e3d676296eb106652a89681ad24034d722894790
trydofor <trydofor@gmail.com>
2024-10-21 15:01:53 +0800

    💚 dummy release to test workflows

---
+++ .changeset/publish-all.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+## changeset publish
+## Tagging and releasing each package is too much
+
+set -x -e
+# pnpm -r publish --access public --report-summary --dry-run --no-git-checks
+pnpm -r publish --access public --report-summary
+
+## simulate https://github.com/changesets/action/blob/31ff97e8acd3cfd08714eab3721f55da10003255/src/run.ts#L139
+## 🦋  New tag:  @fessional/razor-mobile@0.1.24
+jq -r '.publishedPackages[] | "New tag: \(.name)@\(.version)"' pnpm-publish-summary.json
+rm -rf pnpm-publish-summary.json

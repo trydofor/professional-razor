4d7c7502ce0eaaf03eab3de109a86ca721fd92c5
trydofor <trydofor@gmail.com>
2024-10-21 15:01:53 +0800

    💚 dummy release to test workflows

---
+++ .changeset/angry-starfishes-clap.md
@@ -0,0 +1,6 @@
+---
+"@fessional/razor-common": patch
+"@fessional/razor-mobile": patch
+---
+
+💚 dummy release to test workflows
--- .changeset/version-top.sh
+++ .changeset/version-top.sh
@@ -4,6 +4,7 @@ set -x -e
 pnpm exec changeset status --output version-status.tmp
 newv=$(jq -r '.releases[1].newVersion' version-status.tmp)
 
+## replace version in package.json
 if [[ "$(uname)" == "Darwin" ]]; then
   sed -i '' "s/\"version\": \".*\"/\"version\": \"$newv\"/" package.json
 else

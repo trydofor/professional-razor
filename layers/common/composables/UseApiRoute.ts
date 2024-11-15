5676494fd2774ac151fb4cf8e82a23fb7e2aed95
trydofor <trydofor@gmail.com>
2024-11-15 17:34:38 +0800

    ✨ useApiRoute composables #97

---
+++ layers/common/composables/UseApiRoute.ts
@@ -0,0 +1,27 @@
+/**
+ * A helper for handling API fetch with apiRoute prefix.
+ *
+ * @returns {Object} The API helper.
+ * @returns {Function} `url(uri: string): string` - the url apiRoute prefix.
+ * @returns {Function} `get<Data>(uri: string): Promise<DataResult<Data>>` - GET request with apiRoute prefix
+ * @returns {Function} `post<Data>(uri: string): Promise<DataResult<Data>>` - POST request with apiRoute prefix
+ */
+export function useApiRoute() {
+  const prefix = useRuntimeConfig().public.apiRoute;
+  return {
+    url: (uri: string) => prefix + uri,
+    get: <Data>(uri: string, query?: Record<string, unknown>) => $fetch<DataResult<Data>>(
+      prefix + uri,
+      {
+        method: 'get',
+        query,
+      }),
+    post: <Data>(uri: string, body?: Record<string, unknown> | URLSearchParams | FormData, query?: Record<string, unknown>) => $fetch<DataResult<Data>>(
+      prefix + uri,
+      {
+        method: 'post',
+        query,
+        body,
+      }),
+  };
+}

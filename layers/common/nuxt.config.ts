8d1b7f43e4d1cee6c94096da3bdeaa35697ac276
trydofor <trydofor@gmail.com>
2024-12-12 10:40:15 +0800

    ⬆️ refine deps and devDeps #110

--- layers/common/nuxt.config.ts
+++ layers/common/nuxt.config.ts
@@ -4,7 +4,13 @@ import { fileURLToPath } from 'url';
 import { nuxtApiRoutePath, nuxtApiProxyRule, nuxtCompatibilityDate } from './configures/nuxt-config-helper';
 
 export default defineNuxtConfig({
-  modules: ['@unocss/nuxt', '@nuxt/eslint', '@nuxt/test-utils/module', '@pinia/nuxt'],
+  modules: [
+    '@unocss/nuxt',
+    '@nuxt/eslint',
+    '@nuxt/test-utils/module',
+    '@pinia/nuxt',
+    'nuxt-swiper',
+  ],
   // https://nuxt.com/docs/guide/going-further/runtime-config
   runtimeConfig: {
     public: {

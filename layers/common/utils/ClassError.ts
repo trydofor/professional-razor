b6ea162efd1bd93e31e65dfc7945a035866aa087
trydofor <trydofor@gmail.com>
2025-06-01 20:23:08 +0800

    🐛 circular reference https://github.com/nuxt/nuxt/issues/26516 gone

--- layers/common/utils/ClassError.ts
+++ layers/common/utils/ClassError.ts
@@ -1,29 +1,6 @@
-export const TypeApiError = 'ApiErrorResult';
-export const TypeApiFalse = 'ApiFalseResult';
-
 /**
- * api result with error or false
- *
- * @class ApiResultError
- * @extends {Error}
+ * unknown system error
  */
-export class ApiResultError extends Error {
-  public falseResult: DataResult | undefined | null;
-  public errorResult: ErrorResult | undefined | null;
-
-  constructor(result: ApiResult) {
-    if ('errors' in result) {
-      super(result.errors.filter(e => e.message).map(e => e.message).join('\n') || TypeApiError);
-      this.errorResult = result;
-    }
-    else {
-      super(result.message || TypeApiFalse);
-      this.falseResult = result;
-    }
-    Object.setPrototypeOf(this, ApiResultError.prototype);
-  }
-}
-
 export class SystemError extends Error {
   constructor(message: string, public attachment?: unknown) {
     super(message);
